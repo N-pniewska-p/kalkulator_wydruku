e3bcc6d973921260b6f475236ffeddc35bf1d0fd
Nina <ninalia@gmail.com>
2024-12-20 19:05:30 +0100

    few bug fix

--- src/main.py
+++ src/main.py
@@ -21,35 +21,34 @@ def request_to_list(request: str, db : dict):
     order_list=[]
     error_flag=False
     for order in request.split(";"):
-        person = order.split(" ")
+        person = order.strip().split(" ")
         if len (person)!=2:
-            print("Błąd! Poparwny zapis to : \"<format> <ilość>;")
+            print(f"Błąd! '{order.strip()}' nie jest w odpowiednim zapisie. Poparwny zapis to : \"[<format> <ilość>;]\"")
             error_flag=True
             continue
         format = person[0]
         if not person[1].isdigit():
-            print(f"Błąd! {person[1]} nie jest liczbą.")
+            print(f"Błąd! \"{person[1]}\" nie jest liczbą.")
             error_flag=True
             continue
         amount = int(person[1])
         if not format in db:
-            print(f"Bład! Brak formatu {format} w bazie danych")
+            print(f"Bład! Brak formatu \"{format}\" w bazie danych")
             error_flag=True
             continue
         order_list.append(Order(format=format,amount=amount))
-
-    print(order_list)
-
     return error_flag, order_list
 
 def print_format(db):
+    print("Dostępne formaty:")
     for format, price in db.items():
         print(f"format {format} : {price}")
 
 def print_order(list: list):
-    print("")
+    i = 0
     for l in list:
-        print(f"{l.format}:{l.amount}")
+        i += 1
+        print(f"{i}) {l.format} : {l.amount}")
     
 def main(): 
     argv = sys.argv
@@ -70,26 +69,28 @@ def main():
 
         
 
-    print("Cześć jestem kalkultorem wydruków! Jestem zajebisty! Da pan 5!")
+    print("Cześć jestem kalkultorem wydruków!")
    
     print_format(db)
     the_list=[]
     while True:
-
-        request = input("Podaj format i ilość : ")
+        request = input("Wprowadź komędę: \n[<format> <ilość>;] | Podsumuj | Clear | Quit\n")
         if request.strip().lower()=="podsumuj":
             finaly=calculete_final_price(the_list, db)
             print(f"Twoja ostateczna cena to: {finaly:.2f}")
             continue 
         elif request.strip().lower()=="quit":
             exit()
+        elif request.strip().lower()=="clear":
+            the_list.clear()
+            continue            
         
         error_flag,temp_list=request_to_list(request=request, db=db)
-        if error_flag:
-
+        if error_flag and bool(temp_list):
+            print("Poprawne elementy:")
             print_order(temp_list)
             while True:
-                answer=input("Czy dodać poprawne elmenty? Tak/Nie\n")
+                answer=input("Czy dodać poprawne elmenty? Tak | Nie\n")
                 if answer.strip().lower()=="tak": 
                     break
                 elif answer.strip().lower()=="nie":
@@ -100,6 +101,7 @@ def main():
                 else:
                     print("Błąd! Tylko odpowiedzi Tak/Nie.")
         the_list.extend(temp_list)
+        print("Lista dodanych pozycji:")
         print_order(the_list)
 
 main()
